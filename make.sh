7532ac216df81ea3071cb7e1d0531aea252a39fb
Dirmeier  Simon <simondi@bs-mbpr134.d.ethz.ch>
2015-10-02 17:58:30 +0200

    added mac support

--- make.sh
+++ make.sh
@@ -3,8 +3,12 @@ if [ ! -d "$DIGAL" ]; then
 	echo "Creating directory: $DIGAL"
 	sudo mkdir -p $DIGAL
 fi
+if [ "$(uname)" == "Darwin" ]; then
+ 	sed -i '' '/digital_alexandria/d' ~/.zshrc 
+else   
+	sed --in-place=.bak '/digital_alexandria/d' ~/.zshrc
+fi
 
-sed --in-place=.bak '/digital_alexandria/d' ~/.zshrc
 export PATH="$DIGAL:$PATH"
 echo "export PATH=$DIGAL:$PATH" >> ~/.zshrc
 
