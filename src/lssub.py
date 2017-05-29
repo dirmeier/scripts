3d5389aa941fc4493ed5d8e746f4af3201b4146b
dirmeier <simon.dirmeier@web.de>
2017-05-29 19:21:58 +0200

    Added regex instead of flat string match for lssub

--- src/lssub.py
+++ src/lssub.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function, absolute_import
 import os, sys
-
+import re
 
 def usage():
     s = """Print the number of files in the lowest sub-folder level of a specified folder.
@@ -23,13 +23,13 @@ def main(args):
     if len(args) != 2:
         usage()
     pattern, folder = args[0].lower(), args[1]
+    reg = re.compile(pattern)
     for d, s, f in os.walk(folder):
         if len(s):
             continue
-        files = filter(lambda x: pattern in x.lower(), f)
+        files = filter(lambda x: reg.match(x.lower()), f)
         if len(files):
-            print("Directory ", d, ": ", len(files))
-
+            print("Directory ", d, ": ", len(files), ", first file: " , files[0])
 
 if __name__ == "__main__":
     main(sys.argv[1:])
